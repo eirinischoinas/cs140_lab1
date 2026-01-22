a04f57c4bb7d7c4ffdcd43901ac79fd8e623912a
Khilan Surapaneni <ksurapaneni@ucsb.edu>
2026-01-21 17:19:10 -0800

    question 2a done

--- README.txt
+++ README.txt
@@ -2,14 +2,17 @@ Last name of Student 1: Surapaneni
 First name of Student 1: Khilan
 Email of Student 1: ksurapaneni@ucsb.edu
 Last name of Student 2: Schoinas
-First name of Student 2: Renis
+First name of Student 2: Eirini
 Email of Student 2: eirini@ucsb.edu
 
 
 
 Report for Question 2.a
 
-
+For the upper-triangular case with n=4096 and t=1024, the single-process baseline completed in 10.43 seconds at 1.65 GFLOPS. 
+Running with 2 MPI processes reduced runtime to 7.62 seconds (2.25 GFLOPS), giving a speedup of about 1.37x, 
+while 4 processes further reduced runtime to 4.28 seconds (4.01 GFLOPS), a speedup of roughly 2.43x. 
+This corresponds to efficiencies of approximately 68% on 2 cores and 61% on 4 cores. 
 
 Report for Question 2.b
 
--- itmv_mult_test_mpi.c
+++ itmv_mult_test_mpi.c
@@ -419,17 +419,20 @@ char *itmv_test14() {
  * Run all tests.  Ignore returned messages.
  */
 void run_all_tests(void) {
-  mu_run_test(itmv_test1);
-  mu_run_test(itmv_test2);
-  mu_run_test(itmv_test3);
-  mu_run_test(itmv_test4);
-  mu_run_test(itmv_test5);
-  mu_run_test(itmv_test6);
-  mu_run_test(itmv_test7);
-  mu_run_test(itmv_test8);
-  /* mu_run_test(itmv_test9); mu_run_test(itmv_test10);
-     mu_run_test(itmv_test11); mu_run_test(itmv_test12);
-     mu_run_test(itmv_test13); mu_run_test(itmv_test14); */
+  // mu_run_test(itmv_test1);
+  // mu_run_test(itmv_test2);
+  // mu_run_test(itmv_test3);
+  // mu_run_test(itmv_test4);
+  // mu_run_test(itmv_test5);
+  // mu_run_test(itmv_test6);
+  // mu_run_test(itmv_test7);
+  // mu_run_test(itmv_test8);
+  // mu_run_test(itmv_test9);
+  // mu_run_test(itmv_test10);
+  // mu_run_test(itmv_test11);
+  mu_run_test(itmv_test12);
+  // mu_run_test(itmv_test13);
+  // mu_run_test(itmv_test14);
 }
 
 /*-------------------------------------------------------------------
