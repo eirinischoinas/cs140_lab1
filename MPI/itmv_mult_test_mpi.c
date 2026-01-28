5d037c2188365d4da9cb128cd845167dbd33387c
Khilan Surapaneni <ksurapaneni@ucsb.edu>
2026-01-28 15:20:01 -0800

    uncommented test 12

--- MPI/itmv_mult_test_mpi.c
+++ MPI/itmv_mult_test_mpi.c
@@ -430,7 +430,7 @@ void run_all_tests(void) {
   // mu_run_test(itmv_test9);
   // mu_run_test(itmv_test10);
   mu_run_test(itmv_test11);
-  // mu_run_test(itmv_test12);
+  mu_run_test(itmv_test12);
   // mu_run_test(itmv_test13);
   // mu_run_test(itmv_test14);
 }
